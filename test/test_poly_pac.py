f7ed9258f30f72fde12ed204b66c8a3b7d4eb144
Sebastian Martinez <sebasrseco@gmail.com>
2026-07-18 17:11:31 -0400

    test: Agregar mas pruebas para saber si la clase Polynomial cumple con las propiedades de un polinomio real.

--- test/test_poly_pac.py
+++ test/test_poly_pac.py
@@ -3,4 +3,33 @@ from poly_pac import Polynomial
 def test_polynomial_str():
     p = Polynomial([1, 0, -2, 3])
     assert str(p) == "1 - 2x^2 + 3x^3"
-    assert p.degree() == 3
+
+def test_polynomial_degree():
+    p = Polynomial([0, 0, 0])
+    assert p.degree() == 2
+    p = Polynomial([1, 2, 3])
+    assert p.degree() == 2
+
+import pytest
+def test_equality():
+    assert Polynomial((0, 1)) == Polynomial((0, 1))
+
+
+@pytest.mark.parametrize(
+    "a, b, sum",
+    (((0,), (0, 1), (0, 1)),
+     ((2, 0, 3), (1, 2), (3, 2, 3)),
+     ((4, 2), (10, 2, 4), (14, 4, 4)))
+    )
+def test_add(a, b, sum):
+    assert Polynomial(a) + Polynomial(b) == Polynomial(sum)
+
+def test_add_scalar():
+    assert Polynomial((2, 1)) + 3 == Polynomial((5, 1))
+
+def test_reverse_add_scalar():
+    assert 3 + Polynomial((2, 1)) == Polynomial((5, 1))
+
+def test_add_unknown():
+    with pytest.raises(TypeError):
+        Polynomial((1,)) + "frog"
